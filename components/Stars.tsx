1d9447c4592f023e023aa36254a45433cbfa9328
Bhumika <bhumikarajvanshi7@gmail.com>
2026-01-06 17:18:39 +0545

    added stars

---
+++ components/Stars.tsx
@@ -0,0 +1,28 @@
+export default function Stars() {
+  const stars = Array.from({ length: 150 });
+
+  return (
+    <div className="absolute inset-0">
+      {stars.map((_, i) => {
+        const size = Math.random() * 2 + 1;
+        const top = Math.random() * 100;
+        const left = Math.random() * 100;
+        const opacity = Math.random();
+
+        return (
+          <div
+            key={i}
+            className="absolute bg-white rounded-full"
+            style={{
+              width: size,
+              height: size,
+              top: `${top}%`,
+              left: `${left}%`,
+              opacity,
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+}
--- components/Universe.tsx
+++ components/Universe.tsx
@@ -1,9 +1,11 @@
 
 
+import Stars from "./Stars";
+
 export default function Universe() {
   return (
     <div className="fixed inset-0 bg-black overflow-hidden">
-      {/* The universe begins here */}
+      <Stars />
     </div>
   );
 }
