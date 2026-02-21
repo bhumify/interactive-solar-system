94b8d917a0341a33ae5db0093473af34cd93d1c0
Bhumika <bhumikarajvanshi7@gmail.com>
2026-02-21 19:55:58 +0545

    fix top property

--- components/Universe.tsx
+++ components/Universe.tsx
@@ -1,16 +1,15 @@
-
+"use client";
 import React from "react";
 import Stars from "./Stars";
 import Shootingstars from "./Shootingstars";
 import SolarSystem from "./SolarSystem";
 
 const Universe: React.FC = () => {
-  
   const shootingStarsData = [
-    { left: 10, direction: 1, angle: 45, duration: 2, id: 1 },
-    { left: 30, direction: -1, angle: 60, duration: 3, id: 2 },
-    { left: 70, direction: 1, angle: 30, duration: 1.5, id: 3 },
-    { left: 50, direction: -1, angle: 50, duration: 2.5, id: 4 },
+    { top: 10, left: 10, direction: 1, angle: 45, duration: 2, id: 1 },
+    { top: 30, left: 30, direction: -1, angle: 60, duration: 3, id: 2 },
+    { top: 50, left: 70, direction: 1, angle: 30, duration: 1.5, id: 3 },
+    { top: 20, left: 50, direction: -1, angle: 50, duration: 2.5, id: 4 },
   ];
 
   return (
@@ -23,4 +22,3 @@ const Universe: React.FC = () => {
 };
 
 export default Universe;
-
